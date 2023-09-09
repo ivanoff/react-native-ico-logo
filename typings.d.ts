05f30b4ae4c31751039552588dcbc8f30f00d221
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:46:03 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -101,10 +101,35 @@ declare module 'react-native-ico-logo' {
       'after-effects' |
       'adidas';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -112,4 +137,3 @@ declare module 'react-native-ico-logo' {
     export { iconNames };
     export default Icon;
 }
-
